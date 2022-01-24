c410802b2f8f1820d91394932035f8696ff8a7a6
Pascal Visser <p.visser@st.hanze.nl>
2022-01-24 13:49:17 +0100

    minor changes

--- CalorieTracker/src/main/java/CalorieTracker/webapp/config/WebConfig.java
+++ CalorieTracker/src/main/java/CalorieTracker/webapp/config/WebConfig.java
@@ -1,24 +1,64 @@
 package CalorieTracker.webapp.config;
 
+import CalorieTracker.webapp.dao.DatabaseException;
+import CalorieTracker.webapp.dao.MyAppDaoFactory;
+
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServletResponse;
 
-public class WebConfig {
-    public static TemplateEngine createTemplateEngine(ServletContext servletContext) {
+
+@WebListener
+public class WebConfig implements ServletContextListener {
+
+    private static TemplateEngine templateEngine;
+
+    public static void setTemplateEngine(TemplateEngine templateEngine) {
+        WebConfig.templateEngine = templateEngine;
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("[WebConfig] Initializing template engine");
+        createTemplateEngine(servletContextEvent.getServletContext());
+
+        try {
+            MyAppDaoFactory.initializeDataSource();
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("Shutting down!");
+    }
+
+  public static TemplateEngine createTemplateEngine(ServletContext servletContext) {
         ServletContextTemplateResolver templateResolver =
                 new ServletContextTemplateResolver(servletContext);
-        templateResolver.setTemplateMode("XHTML");
+        templateResolver.setTemplateMode("HTML");
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setCacheTTLMs(3600000L);
         // Cache is set to true by default.
         // Set to false if you want templates to be automatically
         // updated when modified.
-        templateResolver.setCacheable(false);
+        templateResolver.setCacheable(true);
         TemplateEngine templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    /**
+     * serves the template engine that was created at application startup.
+     * @return
+     */
+    public static TemplateEngine getTemplateEngine() {
         return templateEngine;
     }
 
@@ -31,6 +71,6 @@ public class WebConfig {
         response.setHeader("Pragma", "no-cache");
         response.setHeader("Cache-Control", "no-cache");
         response.setDateHeader("Expires", 0);
-    }
 
+    }
 }
