8c530caa72519f50a00c30fdcf129e15f6cfabf6
Pascal Visser <p.visser@st.hanze.nl>
2022-01-24 13:50:44 +0100

    Added function for user Validation in doPost

--- CalorieTracker/src/main/java/CalorieTracker/webapp/servlets/WelcomeServlet.java
+++ CalorieTracker/src/main/java/CalorieTracker/webapp/servlets/WelcomeServlet.java
@@ -1,7 +1,11 @@
 package CalorieTracker.webapp.servlets;
 
 import CalorieTracker.webapp.config.WebConfig;
+import CalorieTracker.webapp.model.Validate;
+import com.mysql.cj.protocol.x.XMessage;
 import org.thymeleaf.context.WebContext;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,11 +16,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serial;
 
-@WebServlet(name = "WelcomeServlet", urlPatterns = {"/home"}, loadOnStartup = 1)
+@WebServlet(name = "WelcomeServlet", loadOnStartup = 1)
 public class WelcomeServlet extends HttpServlet {
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         System.out.println("Initializing Thymeleaf template engine");
         final ServletContext servletContext = this.getServletContext();
         WebConfig.createTemplateEngine(servletContext);
@@ -25,18 +29,41 @@ public class WelcomeServlet extends HttpServlet {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
-        response.setContentType("text/html");
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+
+        WebContext ctx = new WebContext(
+                request,
+                response,
+                request.getServletContext(),
+                request.getLocale());
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        try {
+
+            if(Validate.checkUser(username, password))
+            {
+                RequestDispatcher rd = request.getRequestDispatcher("food_input");
+                rd.forward(request, response);
+            }
+            else
+            {
+                System.out.println("check1");
+                ctx.setVariable("ErrorMessage", "Error, wrong username and/or password!");
+                System.out.println("check2");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+        process(request, response);
+    }
 
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         process(request, response);
     }
 
@@ -52,5 +79,4 @@ public class WelcomeServlet extends HttpServlet {
         WebConfig.createTemplateEngine(getServletContext()).
                 process("home", ctx, response.getWriter());
     }
-
 }
