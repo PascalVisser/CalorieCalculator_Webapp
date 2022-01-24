3559bea4ae3540935556bdf7d5cdb260840cc832
Pascal Visser <p.visser@st.hanze.nl>
2022-01-24 13:53:10 +0100

    Added .jar contents as example

---
+++ CalorieTracker/src/main/java/CalorieTracker/webapp/DButils/PasswordRetrievalException.java
@@ -0,0 +1,12 @@
+package CalorieTracker.webapp.DButils;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class PasswordRetrievalException extends Exception {
+    public PasswordRetrievalException(String arg0) {
+        super(arg0);
+    }
+}
