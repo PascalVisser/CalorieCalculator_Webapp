0a61cf7712ba16307304785971f0559ded44c541
Pascal Visser <p.visser@st.hanze.nl>
2022-02-10 10:08:07 +0100

    added inline comments

--- CalorieTracker/src/main/java/CalorieTracker/webapp/Database_IC/EmptyDB.java
+++ CalorieTracker/src/main/java/CalorieTracker/webapp/Database_IC/EmptyDB.java
@@ -1,3 +1,9 @@
+/**
+ * Script for delete records of a database table
+ * @author Pascal Visser
+ * @version 1.0
+ */
+
 package CalorieTracker.webapp.Database_IC;
 
 import java.sql.Connection;
@@ -11,15 +17,16 @@ public class EmptyDB {
 
     public static void EmptyDatabase (String check) throws SQLException, ClassNotFoundException {
 
+        // Make connection
         Connection connect = Validate.DBConnect();
 
+        // Try to delete contents of table or throw error
         if (Objects.equals(check, "True")) {
-
             Statement stmt = connect.createStatement();
                     stmt.execute("DELETE FROM test_input");
             System.out.println("done!");
         } else {
-            System.out.println("nope");
+            System.out.println("Error, Something went wrong");
         }
     }
 }
